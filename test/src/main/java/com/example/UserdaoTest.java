1fa62a64bc7154fa719ceb520ff03f2e3cf8a86f
Jangchulwoon <lusiue@naver.com>
2016-02-19 22:29:37 +0900

    creat error list and revise some class

--- test/src/main/java/com/example/UserdaoTest.java
+++ test/src/main/java/com/example/UserdaoTest.java
@@ -24,14 +24,16 @@ import ioc_Test.UserDao;
 public class UserdaoTest {
 	@Autowired
 	private ApplicationContext app;
+	@Autowired
 	UserDao dao;
+	// autowired는 빈중에 해당 타입이 있으면 자동으로 연결해줌 .. 
+	// getbean을 굳이 쓸 필요 x 
 	User user1;
 	User user2;
 	User user3;
 
 	@Before
 	public void setUp() {
-		dao = this.app.getBean("userdao", UserDao.class);
 		// context 가 반복해서 만들어 지기 때문에 , 이게 많아지면 시간이 상승 ! 어떻게 해결 ?
 		user1 = new User("id", "장철운", "1234");
 		user2 = new User("id1", "장철운", "1234");
